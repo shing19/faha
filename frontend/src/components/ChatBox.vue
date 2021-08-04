b3fa7f4c303df6d3943c145917ee25a779d2e51e
Shing <shing19@outlook.com>
2021-08-04 21:18:02 +0800

    KanBanNiang+img show box

--- frontend/src/components/ChatBox.vue
+++ frontend/src/components/ChatBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="all">
     <section class="chat-box">
       <div class="chat-box-list-container" ref="chatbox">
         <ul class="chat-box-list">
@@ -12,7 +12,7 @@
             <div v-show="(message.image==null)">
               <span v-show="!(message.text==null)">{{ message.text }}</span>
             </div>
-            <img :id="idx" :src="message.image" v-show="!(message.image==null)" alt="image" style="width:65%"/>
+            <img :id="idx" :src="message.image" v-show="!(message.image==null)" alt="image" style="width:65%" @load="scroll"/>
           </li>
         </ul> 
       </div>
@@ -24,16 +24,27 @@
         />
         <button @click="sendMessage">Send</button>
       </div>
+      <button class="restart" v-on:click="restart">restart</button>
+      <input class="file" name="file" type="file" accept="image/png,image/gif,image/jpeg" @change="upload"/>
     </section>
-    <button class="restart" v-on:click="restart">restart</button>
-    <input class="file" name="file" type="file" accept="image/png,image/gif,image/jpeg" @change="upload"/>
-    <!-- <input class="file" name="file" type="file" accept="image/png,image/gif,image/jpeg"/> -->
-  </div>
-</template>
+    <section class="img-box" ref="imgbox">
+      <div class="img-box-list-container" ref="imgbox">
+        <ul class="img-box-list">
+          <li 
+            class="img"
+            v-for="(message, idx) in messages"
+            :key="idx"
+          >
+            <img :id="idx" :src="message.image" v-show="!(message.image==null)" alt="image" style="width: 100%" @load="scroll"/>
+          </li>
+        </ul> 
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import {L2Dwidget} from 'live2d-widget'
 export default {
   name: 'ChatBox',
   data: () => ({
@@ -41,7 +52,7 @@ export default {
     text: '',
     messages: [],
     sender_id: '',
-    filename: ''
+    filename: '',
   }),
   mounted() {
       this.sender_id = uuidv4()
@@ -67,13 +78,14 @@ export default {
         console.log(this.text)
         this.$nextTick(() => {
           this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+          this.$refs.imgbox.scrollTop = this.$refs.imgbox.scrollHeight
         })
         this.$axios.post('http://1.117.208.226:5005/webhooks/rest/webhook', {
         // this.$axios.post('http://1.116.3.150:5005/webhooks/rest/webhook', {
         // this.$axios.post('http://localhost:5005/webhooks/rest/webhook', {
           "message": this.text,
           "sender": this.sender_id
-        })
+        }, {timeout: 1000 * 60 * 4})
         .then(res => {
           this.messages.push({
             text: res.data[0].text,
@@ -81,9 +93,10 @@ export default {
             author: 'server'
           })
           console.log('server says: ')
-          console.log(res.data[0]['text'])
+          console.log(res.data[0])
           this.$nextTick(() => {
             this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+            this.$refs.imgbox.scrollTop = this.$refs.imgbox.scrollHeight
           })
         })
       }
@@ -92,11 +105,12 @@ export default {
       this.$axios.post('http://1.117.208.226:5005/webhooks/rest/webhook', {
           "message": '/restart',
           "sender": 'client'
-      })
+      }, {timeout: 1000 * 60 * 4})
       .then(res => {
         this.messages = []
         this.$nextTick(() => {
           this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+          this.$refs.imgbox.scrollTop = this.$refs.imgbox.scrollHeight
           this.sender_id = uuidv4()
           console.log('update sender id: '+this.sender_id)
           function uuidv4() {
@@ -113,7 +127,7 @@ export default {
       let param = new FormData() //创建form对象
       param.append('file',file) //通过append向form对象添加数据
       console.log(param.get('file')) //FormData私有类对象，访问不到，可以通过get判断值是否传进去
-      this.$axios.post('http://1.117.208.226:8000/api/upload',param,{headers:{'Content-Type':'application/x-www-form-urlencoded' }}, ) //请求头要为表单
+      this.$axios.post('http://1.117.208.226:8000/api/upload',param,{headers:{'Content-Type':'application/x-www-form-urlencoded' }}, {timeout: 1000 * 60 * 4} ) //请求头要为表单
       .then(response=>{
         console.log(response.data)
         let image_src = 'http://1.117.208.226:8000/static/images/' + response.data['filename']
@@ -122,16 +136,17 @@ export default {
           author: 'client'
         })
         this.filename = response.data['filename']
-        console.log('client says: upload ')
-        console.log(this.filename)
+        console.log('client says: ')
+        console.log('upload_image: ' + this.filename)
         this.$nextTick(() => {
           this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+          this.$refs.imgbox.scrollTop = this.$refs.imgbox.scrollHeight
         })
         this.text = 'upload_image: ' + this.filename 
         this.$axios.post('http://1.117.208.226:5005/webhooks/rest/webhook', {
           "message": this.text,
           "sender": this.sender_id
-        })
+        }, {timeout: 1000 * 60 * 4})
         .then(res => {
           this.messages.push({
             text: res.data[0].text,
@@ -139,9 +154,10 @@ export default {
             author: 'server'
           })
           console.log('server says: ')
-          console.log(res.data[0]['text'])
+          console.log(res.data[0])
           this.$nextTick(() => {
             this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+            this.$refs.imgbox.scrollTop = this.$refs.imgbox.scrollHeight
           })
         })
       })
@@ -149,6 +165,29 @@ export default {
         console.log(error)
       })
     },
+    scroll(e) {
+      this.$nextTick(() => {
+        this.$refs.chatbox.scrollTop = this.$refs.chatbox.scrollHeight
+        this.$refs.imgbox.scrollTop = this.$refs.imgbox.scrollHeight
+      })
+    },
+  },
+  created() {
+      setTimeout(function () {
+          L2Dwidget.init({
+              model: {
+                jsonPath: 'https://cdn.jsdelivr.net/gh/wangsrGit119/wangsr-image-bucket/L2Dwidget/live2d-widget-model-miku/assets/miku.model.json',
+                scale: 1.5
+              },
+              display: {
+                position: 'left',
+                width: 300,
+                height: 600,
+                hOffset: 50,
+                vOffset: 10,
+              }
+          })
+      },1000)
   }
 }
 
@@ -156,7 +195,20 @@ export default {
 </script>
 
 <style scoped>
-.chat-box,
+.all {
+  display: inline-flex;
+}
+.chat-box {
+  display: flex;
+  flex-direction: column;
+  width: 40rem;
+  height: 80vh;
+  border: 1px solid #999;
+  border-radius: 4px;
+  margin-left: 25%;
+  margin-right: 3%;
+  justify-content: space-between;
+}
 .chat-box-list {
   display: flex;
   flex-direction: column;
@@ -165,14 +217,6 @@ export default {
 .chat-box-list-container{
   overflow-y: auto;
   scroll-behavior: smooth;
-}
-.chat-box {
-  width: 50%;
-  height: 80vh;
-  border: 1px solid #999;
-  border-radius: 4px;
-  margin-left: 10%;
-  justify-content: space-between;
 }
 .chat-box-list {
   margin-top: 1rem;
@@ -231,11 +275,31 @@ export default {
 }
 .restart {
   position: absolute;
-  left: 10%;
+  left: 29%;
+  bottom: 3%;
+  margin: 0.8rem;
 }
 .file {
   position: absolute;
-  left: 20%;
+  left: 34%;
+  bottom: 3%;
+  margin: 0.8rem;
+}
+.img-box {
+  display: flex;
+  flex-direction: column;
+  width: 35rem;
+  height: 80vh;
+  border: 1px solid #999;
+  border-radius: 4px;
+}
+.img-box-list-container{
+  overflow-y: auto;
+  scroll-behavior: smooth;
+}
+.img-box-list {
+  list-style-type: none;
+  padding: 0.5rem;
 }
 </style>
 
